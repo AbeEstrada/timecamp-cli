5e363813ff1e26f4c16517cb5b0e8199d39f8032
Abe Estrada <abraham.estrada@gmail.com>
2024-03-05 23:06:57 -0700

    Start timer command

---
+++ cmd/timersStart.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start a timer",
+	Run: func(cmd *cobra.Command, args []string) {
+		apiToken := os.Getenv("TIMECAMP_API_TOKEN")
+		if apiToken == "" {
+			fmt.Println("Error: Missing TIMECAMP_API_TOKEN environment variable")
+			return
+		}
+
+		url := "https://app.timecamp.com/third_party/api/timer"
+
+		payload := strings.NewReader("{\"action\":\"start\"}")
+
+		req, _ := http.NewRequest("POST", url, payload)
+
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Authorization", "Bearer "+apiToken)
+
+		res, _ := http.DefaultClient.Do(req)
+
+		defer res.Body.Close()
+		body, _ := io.ReadAll(res.Body)
+		if res.StatusCode != http.StatusOK {
+			var errorMessage ErrorMessage
+			err := json.Unmarshal([]byte(body), &errorMessage)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			fmt.Println("Error:", errorMessage.Message)
+			return
+		}
+
+		type Response struct {
+			EntryID int `json:"entry_id"`
+		}
+
+		var parsedData Response
+		err := json.Unmarshal(body, &parsedData)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		timerID := parsedData.EntryID
+		fmt.Println("Timer ID:", timerID)
+	},
+}
+
+func init() {
+	timersCmd.AddCommand(startCmd)
+}
